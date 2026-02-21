9fe66dc6530b8660479e3f5ead4bd7774b56f1a1
Awais Farooq <awaisfarooq010@gmail.com>
2026-02-22 02:42:57 +0500

    Refactor Firebase messaging service worker to use dynamic configuration and add firebase-env.js for environment variables

---
+++ public/firebase-env.js
@@ -0,0 +1,8 @@
+self.FIREBASE_CONFIG = {
+  apiKey: self.FIREBASE_API_KEY,
+  authDomain: self.FIREBASE_AUTH_DOMAIN,
+  projectId: self.FIREBASE_PROJECT_ID,
+  storageBucket: self.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: self.FIREBASE_MESSAGING_SENDER_ID,
+  appId: self.FIREBASE_APP_ID,
+};
